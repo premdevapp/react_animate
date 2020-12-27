e2c8dfbda964471646a23b8335d7e1039b92eec8
premnath <premdevapp@gmail.com>
2020-12-27 08:14:21 +0530

    transition-group

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import "./App.css";
 import Modal from "./components/Modal/Modal";
 import Backdrop from "./components/Backdrop/Backdrop";
 import List from "./components/List/List";
+import Transition from "react-transition-group/Transition";
 
 class App extends Component {
   state = {
     modalIsOpen: false,
+    showBlock: false,
   };
 
   modalShowHandler = () => {
@@ -26,8 +28,41 @@ class App extends Component {
     return (
       <div className="App">
         <h1>React Animations</h1>
-        <Modal show={this.state.modalIsOpen} closed={this.modalClosedHandler} />
-        <Backdrop show={this.state.modalIsOpen} />
+        <button
+          className="Button"
+          onClick={() =>
+            this.setState((prevState) => ({ showBlock: !prevState.showBlock }))
+          }
+        >
+          Toggle
+        </button>
+        <br />
+        <Transition
+          in={this.state.showBlock}
+          timeout={300}
+          mountOnEnter
+          unmountOnExit
+        >
+          {(state) => (
+            <div
+              style={{
+                backgroundColor: "red",
+                width: "100px",
+                height: "100px",
+                margin: "auto",
+                transition: "opacity 1s ease-out",
+                opacity: state === "exiting" ? 0 : 1,
+              }}
+            >
+              {" "}
+            </div>
+          )}
+        </Transition>
+        <br />
+        {this.state.modalIsOpen && (
+          <Modal show closed={this.modalClosedHandler} />
+        )}
+        {this.state.modalIsOpen && <Backdrop show />}
         <button className="Button" onClick={this.modalShowHandler}>
           Open Modal
         </button>
