cdd0a920077ef0a5c1b3a1572e99a9a4b748f695
premnath <premdevapp@gmail.com>
2020-12-27 09:25:10 +0530

    y

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import "./App.css";
 import Modal from "./components/Modal/Modal";
 import Backdrop from "./components/Backdrop/Backdrop";
 import List from "./components/List/List";
-import Transition from "react-transition-group/Transition";
+//import Transition from "react-transition-group/Transition";
+import CSSTransition from "react-transition-group/CSSTransition";
 
 class App extends Component {
   state = {
@@ -37,11 +38,29 @@ class App extends Component {
           Toggle
         </button>
         <br />
-        <Transition
+        {/* <Transition
           in={this.state.showBlock}
           timeout={300}
           mountOnEnter
           unmountOnExit
+          onEnter={() => {
+            console.log("onEnter");
+          }}
+          onEntering={() => {
+            console.log("onEntering");
+          }}
+          onEntered={() => {
+            console.log("onEntered");
+          }}
+          onExit={() => {
+            console.log("onExit");
+          }}
+          onExiting={() => {
+            console.log("onExiting");
+          }}
+          onExited={() => {
+            console.log("onExited");
+          }}
         >
           {(state) => (
             <div
@@ -57,7 +76,52 @@ class App extends Component {
               {" "}
             </div>
           )}
-        </Transition>
+        </Transition> */}
+        <CSSTransition
+          in={this.state.showBlock}
+          timeout={1000}
+          mountOnEnter
+          unmountOnExit
+          onEnter={() => {
+            console.log("onEnter");
+          }}
+          onEntering={() => {
+            console.log("onEntering");
+          }}
+          onEntered={() => {
+            console.log("onEntered");
+          }}
+          onExit={() => {
+            console.log("onExit");
+          }}
+          onExiting={() => {
+            console.log("onExiting");
+          }}
+          onExited={() => {
+            console.log("onExited");
+          }}
+          //classNames="fade-slide"
+          classNames={{
+            enter: "",
+            enterActive: "fade-slide-enter-active",
+            exit: "",
+            exitActive: "fade-slide-exit-activeal",
+            //appear:
+            //appearActive:
+          }}
+        >
+          <div
+            style={{
+              backgroundColor: "red",
+              width: "100px",
+              height: "100px",
+              margin: "auto",
+              transition: "opacity 1s ease-out",
+            }}
+          >
+            {" "}
+          </div>
+        </CSSTransition>
         <br />
 
         <Modal show={this.state.modalIsOpen} closed={this.modalClosedHandler} />
--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -4,9 +4,19 @@ import "./Modal.css";
 
 import Transition from "react-transition-group/Transition";
 
+const animationTiming = {
+  enter: 400,
+  exit: 1000,
+};
+
 const modal = (props) => {
   return (
-    <Transition in={props.show} timeout={300} mountOnEnter unmountOnExit>
+    <Transition
+      in={props.show}
+      timeout={animationTiming}
+      mountOnEnter
+      unmountOnExit
+    >
       {(state) => {
         const cssClasses = [
           "Modal",
